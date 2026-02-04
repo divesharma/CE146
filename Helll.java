70cac7549bfb6a937d51e30bb45dcd4c18d4ad84
divesharma <themundirectory@gmail.com>
2026-02-04 09:30:22 +0530

    Add files via upload

---
+++ Helll.java
@@ -0,0 +1,21 @@
+class Helll
+{
+    public static void main (Strings [] agrs)
+    {
+        int a=10;
+        int b=15;
+        int c=20;
+        if (a>b && a>c)
+        {
+            System.out.println("a is max");
+        }
+        else if (b>a && b>c)
+        {
+            System.out.println("b is max");
+        }
+        else 
+        {
+            System.out.println("c is max");
+        }
+    }
+}
